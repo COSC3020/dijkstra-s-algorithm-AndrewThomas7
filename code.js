7d4a204889f9d436fb145516d09fdcb4247e87a6
AndrewThomas7 <168784458+AndrewThomas7@users.noreply.github.com>
2025-05-09 21:16:44 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,72 @@
-function dijkstra(graph, sourceNode) {
-    return [];
+function FindSmallestEdge(node){
+    var NonZeroList=[]    
+    var smallest;
+    for(var x=0;x<node.length;x++){
+        if(node[x]!=0&&node[x]!=Infinity){
+            NonZeroList.push(node[x])
+        }
+    }
+    smallest=NonZeroList[0]
+    for(var y=0; y<NonZeroList.length;y++){
+        if(NonZeroList.length==1){
+            smallest=NonZeroList[0]
+            break
+        }
+        if(NonZeroList[y]>NonZeroList[y+1]){
+            smallest=NonZeroList[y+1]
+        }
+    }
+    return smallest
 }
+
+function ArrayCompare(array,no){
+    var Equal=false;
+    var count=0;
+    for(var m=0; m<array.length;m++){
+        if(array[m]==no){
+            count++
+        }
+    }
+    if(count>=array.length){
+        Equal=true
+    }
+    return Equal
+}
+function Dijisktras(graph,sourceNode){
+    var Distancelist=new Array(graph.length).fill(Infinity)
+    var Visited= new Array(graph.length).fill(0)
+    var edgelist=[]
+    var smallest=0;
+    Distancelist[sourceNode]=0;
+    function Dijisktras_(node){
+        Visited[node]=1;
+        edgelist=graph[node];
+        smallest=FindSmallestEdge(edgelist);
+
+        if(ArrayCompare(Visited,1)){
+            return
+        }    
+        for(var x=0; x<edgelist.length;x++){
+            if(edgelist[x]!=Infinity&& edgelist[x]!=0&& node!=sourceNode){
+                if(edgelist[x]+Distancelist[node]<=Distancelist[x]){
+                    Distancelist[x]=edgelist[x]+Distancelist[node]
+                }
+            }
+            else if(edgelist[x]!=Infinity&& edgelist[x]!=0){
+                if(edgelist[x]<=Distancelist[x]){
+                    Distancelist[x]=edgelist[x]
+                }
+            }
+        }
+        for( weight of edgelist){
+                if(weight==smallest && Visited[edgelist.indexOf(smallest)]!=1){
+                    Dijisktras_(graph[node].indexOf(weight))
+                }
+                else if(Visited[edgelist.indexOf(smallest)]==1&& Visited[edgelist.indexOf(weight)]!=1){
+                    Dijisktras_(graph[node].indexOf(weight))
+                }
+            }
+        }
+        Dijisktras_(sourceNode)
+        return Distancelist
+    }
