aeabb13abb1d64e15fe471b352280512f924b467
AndrewThomas7 <168784458+AndrewThomas7@users.noreply.github.com>
2025-05-09 21:27:56 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -2,13 +2,21 @@ const fs = require('fs');
 const assert = require('assert');
 eval(fs.readFileSync('code.js')+'');
 
+G=[
+    [0,6,5,Infinity],
+    [Infinity,0,4,3],
+    [Infinity,Infinity,0,2],
+    [Infinity,Infinity,Infinity,0]
+]
+console.assert(JSON.stringify(Dijisktras(G,0))==JSON.stringify([0,6,5,7])
+                                         
 GUndir=[
     [0,6,5,Infinity],
     [6,0,4,3],
     [5,4,0,2],
     [Infinity,3,4,0]
 ]
-
+console.assert(JSON.stringify(Dijisktras(G,
 
 G2=[
     [0,4,2,Infinity,Infinity],
