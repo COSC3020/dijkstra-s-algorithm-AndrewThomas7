a7d106577047578447f4edf10fc4fab1eb1432c9
AndrewThomas7 <168784458+AndrewThomas7@users.noreply.github.com>
2025-05-09 22:16:26 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -28,9 +28,14 @@ G2=[
     [0,4,2,Infinity,Infinity],
     [Infinity,0,3,2,3],
     [Infinity,1,0,4,5],
-    [Infinity,2,4,0,1],
-    [Infinity,3,5,Infinity,0]
+    [Infinity,Infinity,Infinity,0,Infinity],
+    [Infinity,Infinity,Infinity,1,0]
 ]
+console.assert(JSON.stringify(Dijisktras(G2,0))==JSON.stringify([ 0, 3, 2, 5, 6 ]))
+console.assert(JSON.stringify(Dijisktras(G2,1))==JSON.stringify([ Infinity, 0, 3, 2, 3 ]))
+console.assert(JSON.stringify(Dijisktras(G2,2))==JSON.stringify([ Infinity, 1, 0, 3, 4 ]))
+console.assert(JSON.stringify(Dijisktras(G2,3))==JSON.stringify([ Infinity, Infinity, Infinity, 0, Infinity ]))
+sonsole.assert(JSON.stringify(Dijisktras(G2,4))==JSON.stringify([ Infinity, Infinity, Infinity, 1, 0 ]))
 
 G3=[
     [0,7,Infinity,Infinity,Infinity],
@@ -39,3 +44,8 @@ G3=[
     [Infinity,-1,Infinity,0,Infinity],
     [Infinity,Infinity,Infinity,Infinity,0]
 ]
+console.assert(JSON.stringify(Dijisktras(G3,0))==JSON.stringify([0,0,5,1,9]))
+console.assert(JSON.stringify(Dijisktras(G3,1))==JSON.stringify([ Infinity, -7, -2, -6, 2 ]))
+console.assert(JSON.stringify(Dijisktras(G3,2))==JSON.stringify([ Infinity, -5, -7, -4, -3 ]))
+console.assert(JSON.stringify(Dijisktras(G3,3))==JSON.stringify([ Infinity, -1, -3, -7, 1 ]))
+console.assert(JSON.stringify((Dijisktras(G3,4))==JSON.stringify([ Infinity, Infinity, Infinity, Infinity, 0 ]))
